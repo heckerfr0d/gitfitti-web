218049fb80f8eaa847c626edcc3a453f15496c8a
heckerfr0d <hadif_b190513cs@nitc.ac.in>
2021-04-29 14:32:34 +0530

    added features to match the c++ port. Prolly the final build unless someone discovers bugs (very likely :p)

--- qtAbuz.py
+++ qtAbuz.py
@@ -1,14 +1,21 @@
-import sys, datetime, os, git
+import sys
+import datetime
+import os
+import git
 from font import Font
-from PyQt6.QtWidgets import QApplication, QHBoxLayout, QMessageBox, QWidget, QLineEdit, QPushButton, QCheckBox, QGridLayout, QComboBox, QVBoxLayout
+from PyQt6.QtWidgets import QApplication, QHBoxLayout, QLabel, QMessageBox, QSpinBox, QWidget, QLineEdit, QPushButton, QCheckBox, QGridLayout, QComboBox, QVBoxLayout
 from PyQt6.QtGui import QIcon
 
-def getDates(year):
-    jan1 = datetime.datetime(year, 1, 1, 4, 20, 59)
-    onDay = lambda date, day: date + datetime.timedelta(days=(day-date.weekday())%7)
+
+def getDates(year=None):
+    if year:
+        jan1 = datetime.datetime(year=year, month=1, day=1, hour=10, minute=20, second=59)
+    else:
+        jan1 = datetime.datetime.now() - datetime.timedelta(weeks=53)
+        jan1 -= datetime.timedelta(microseconds=jan1.microsecond)
+    def onDay(date, day): return date + datetime.timedelta(days=(day-date.weekday()) % 7)
     first_sunday = onDay(jan1, 6)
     dates = [list() for x in range(7)]
-    # list of weekdays
     for x in range(52 * 7):
         dates[x % 7].append(first_sunday + datetime.timedelta(x))
     return dates
@@ -22,49 +29,91 @@ class App(QWidget):
         vl = QVBoxLayout()
         hl = QHBoxLayout()
         hl2 = QHBoxLayout()
+        hl3 = QHBoxLayout()
         self.y = QComboBox()
         self.name = QLineEdit()
         self.email = QLineEdit()
+        self.passw = QLineEdit()
         self.repo = QLineEdit()
         self.type = QLineEdit()
         self.fonts = QComboBox()
         self.err = QMessageBox()
+        self.nc = QSpinBox()
+        lbl = QLabel('Commits/day:')
         prev = QPushButton('Translate')
+        invert = QPushButton('Invert')
         leggo = QPushButton('Do it')
+        invert.clicked.connect(self.invert)
         leggo.clicked.connect(self.doit)
         prev.clicked.connect(self.textCheck)
-        self.y.addItem('Select year...')
-        for yr in range(2021, 2000, -1):
+        self.name.textChanged[str].connect(self.rmph)
+        self.email.textChanged[str].connect(self.rmph)
+        self.passw.textChanged[str].connect(self.rmph)
+        self.type.textChanged[str].connect(self.rmph)
+        self.repo.textChanged[str].connect(self.rmph)
+        self.y.addItem('Year (default: last 52 weeks)')
+        for yr in range(datetime.datetime.now().year+5, datetime.datetime.now().year-20, -1):
             self.y.addItem(str(yr))
         self.fonts.addItems(os.listdir('Fonts'))
-        self.name.setPlaceholderText('Committer name*')
-        self.email.setPlaceholderText('Committer email*')
-        self.repo.setPlaceholderText('repo url* as https://username:password@github.com/username/reponame')
+        self.name.setPlaceholderText('Committer name')
+        self.email.setPlaceholderText('Committer email')
+        self.passw.setPlaceholderText('Password')
+        self.passw.setEchoMode(QLineEdit.EchoMode.Password)
+        self.repo.setPlaceholderText('Link to repo')
         self.type.setPlaceholderText('Translate text to tile art!')
+        self.nc.setMinimum(1)
+        self.nc.setValue(1)
         self.err.setWindowIcon(QIcon('icon.png'))
         self.err.setWindowTitle('Error!')
         hl.addWidget(self.name)
         hl.addWidget(self.email)
-        hl.addWidget(self.y)
+        hl.addWidget(self.passw)
+        hl3.addWidget(self.repo)
+        hl3.addWidget(self.y)
+        hl3.addWidget(lbl)
+        hl3.addWidget(self.nc)
         hl2.addWidget(self.type)
         hl2.addWidget(self.fonts)
         hl2.addWidget(prev)
+        hl2.addWidget(invert)
         vl.addLayout(hl)
-        vl.addWidget(self.repo)
+        vl.addLayout(hl3)
         vl.addLayout(layout)
         vl.addLayout(hl2)
         vl.addWidget(leggo)
         self.setLayout(vl)
         self.checkM = [list() for i in range(7)]
         for i in range(7):
-            for j in range(51):
+            for j in range(52):
                 m = QCheckBox()
                 layout.addWidget(m, i, j)
                 self.checkM[i].append(m)
+    def rmph(self):
+        self.setStyleSheet('''
+        QLineEdit[text=""]{
+            border: 1px #30363d;
+            border-radius: 3px;
+            padding: 1px 18px 1px 3px;
+            background-color: #1c2128;
+            color: #8b949e;
+        }
+        QLineEdit{
+            border: 1px #30363d;
+            border-radius: 3px;
+            padding: 1px 18px 1px 3px;
+            background-color: #1c2128;
+            color: #f0f6fc;
+        }
+        QLineEdit:hover {
+            border: 0.5px solid #c9d1d9;
+        }
+        QLineEdit:focus {
+            border: 1px solid #c9d1d9;
+        }''')
     def getActiveDates(self, dates):
         ad = []
         for i in range(7):
-            for j in range(51):
+            for j in range(52):
                 if self.checkM[i][j].isChecked():
                     ad.append(dates[i][j])
         return ad
@@ -73,51 +122,44 @@ class App(QWidget):
         try:
             year = int(self.y.currentText())
             dates = self.getActiveDates(getDates(year))
-            author = git.Actor(self.name.text(), self.email.text())
-            if not self.name.text() or not self.email.text():
-                self.err.setText('Did you enter your name and email? 🙄')
-                self.err.exec()
-                return
+        except:
+            dates = self.getActiveDates(getDates())
+        author = git.Actor(self.name.text(), self.email.text())
+        if not self.name.text() or not self.email.text():
+            self.err.setText('Did you enter your name and email? 🙄')
+            self.err.exec()
+            return
 
-            repurl = self.repo.text()
-            repname = repurl.split('/')[-1]
+        repurl = "https://" + self.name.text() + ":" + self.passw.text() + "@" + self.repo.text()[8:]
+        repname = repurl.split('/')[-1].split('.')[0]
+        if not os.path.isdir(repname):
             try:
                 git.cmd.Git().clone(repurl)
             except:
-                self.err.setText('Could not clone the repo. Ensure that the remote repo exists and that you have access to it.\nThe url should be in https://username:password@github.com/username/reponame format')
+                self.err.setText('Could not clone the repo. Ensure that the remote repo exists and that you have access to it.')
                 self.err.exec()
                 return
-            rep = git.Repo.init(repname)
-            for date in dates:
-                filename = date.strftime('%Y-%m-%d-%H-%M-%S')
-                filepath = os.path.join(repname, filename)
-                with open(filepath, 'w') as f:
-                    f.write(filename)
-                rep.index.add([filename])
-                rep.index.commit("GitHub abuz add", author=author, committer=author, author_date=date.isoformat())
-                rep.index.remove([filename])
-                rep.index.commit("GitHub abuz remove", author=author, committer=author, author_date=(date+datetime.timedelta(minutes=1)).isoformat())
-                os.remove(os.path.join(repname, filename))
-                os.removedirs(repname)
-            try:
-                rep.remotes.origin.set_url(repurl)
-            except:
-                rep.create_remote('origin', repurl)
-            try:
-                rep.remotes.origin.push()
-            except:
-                self.err.setText('Error pushing. Verify you have permissions to push to the repo and that the url is in https://username:password@github.com/username/reponame form')
-                self.err.exec()
-                return
-            result = QMessageBox()
-            text = f"Created {len(dates)*2} commits as {name.text()} <{email.text()}> in {repname} : https://{repurl[repurl.find('github.com'):]}"
-            result.setWindowIcon(QIcon('icon.png'))
-            result.setWindowTitle('All Done!')
-            result.setText(text)
-            result.exec()
+        rep = git.Repo.init(repname)
+        for date in dates:
+            for n in range(self.nc.value()):
+                rep.index.commit("committed for the lullzz!!", author=author, committer=author, author_date=date.isoformat())
+        try:
+            rep.remotes.origin.set_url(repurl)
         except:
-            self.err.setText('Bruh select a year 🤦‍♂️')
+            rep.create_remote('origin', repurl)
+        try:
+            rep.remotes.origin.push()
+        except:
+            self.err.setText('Error pushing. Verify you have permissions to push to the repo and that the given credentials are correct')
             self.err.exec()
+            return
+        result = QMessageBox()
+        text = f"Created {len(dates)*2} commits as {self.name.text()} <{self.email.text()}> in {repname} : {self.repo.text()}"
+        result.setWindowIcon(QIcon('icon.png'))
+        result.setWindowTitle('All Done!')
+        result.setText(text)
+        result.exec()
+        os.remove(repname)
 
     def textCheck(self):
         for r in self.checkM:
@@ -127,35 +169,43 @@ class App(QWidget):
         font = Font(os.path.join('Fonts', self.fonts.currentText()), 8)
         try:
             text = repr(font.render_text(text_to_render, 52, 7))
-            print(text)
             text_by_weekday = text.split('\n')
             for i in range(7):
                 for j in range(51):
-                    if text_by_weekday[i][j]=='#':
+                    if text_by_weekday[i][j] == '#':
                         self.checkM[i][j].setChecked(True)
         except:
             self.err.setText('You typed too long :(')
             self.err.exec()
 
+    def invert(self):
+        for r in self.checkM:
+            for m in r:
+                if m.isChecked():
+                    m.setChecked(False)
+                else:
+                    m.setChecked(True)
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     w = App()
     app.setStyleSheet('''
-        QLineEdit{
-            border: 1px #30363d;
-            border-radius: 3px;
-            padding: 1px 18px 1px 3px;
-            background-color: #1c2128;
-            color: #f0f6fc;
-        }
         QLineEdit[text=""]{
             border: 1px #30363d;
             border-radius: 3px;
             padding: 1px 18px 1px 3px;
             background-color: #1c2128;
             color: #8b949e;
+        }
+        QLineEdit{
+            color: #f0f6fc;
+        }
+        QLineEdit:hover {
+            border: 0.5px solid #c9d1d9;
+        }
+        QLineEdit:focus {
+            border: 1px solid #c9d1d9;
         }
         QWidget{
             background-color: #0d1117;
@@ -343,7 +393,62 @@ if __name__ == "__main__":
         {
             background: none;
         }
-
+        QSpinBox {
+            padding-right: 15px; /* make room for the arrows */
+            border: 1px solid #30363d;
+            border-radius: 3px;
+            color: #f0f6fc;
+            background: solid #1c2128
+        }
+        QSpinBox:hover {
+            border: 0.5px solid #c9d1d9;
+        }
+        QSpinBox:focus {
+            border: 1px solid #c9d1d9;
+        }
+        
+        QSpinBox::up-button {
+            subcontrol-origin: border;
+            subcontrol-position: top right; /* position at the top right corner */
+            width: 14px; /* 16 + 2*1px border-width = 15px padding + 3px parent border */
+            border: 1px solid #30363d;
+            border-bottom: 0px;
+            background: solid #30363d;
+        }
+        
+        QSpinBox::up-arrow {
+            width: 7px;
+            height: 7px;
+            background: solid black;
+        }
+        
+        QSpinBox::down-button {
+            subcontrol-origin: border;
+            subcontrol-position: bottom right; /* position at bottom right corner */
+            width: 14px;
+            border: 1px solid #30363d;
+            border-top: 0px;
+            background: solid #30363d;
+        }
+        QSpinBox::down-button:hover {
+        border: 0.5px solid #c9d1d9;
+        }
+        QSpinBox::down-button:focus {
+        border: 1px solid #c9d1d9;
+        border-top: 0px;
+        }
+        QSpinBox::up-button:hover {
+        border: 0.5px solid #c9d1d9;
+        }
+        QSpinBox::up-button:focus {
+        border: 1px solid #c9d1d9;
+        border-bottom: 0px;
+        }
+        QSpinBox::down-arrow {
+            width: 7px;
+            height: 7px;
+            background: solid black;
+        }
     ''')
     w.show()
     sys.exit(app.exec())
