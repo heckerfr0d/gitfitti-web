65fbbab1eb71783c2fd4d06d6de2f0fb4ee8041c
batman <batman@gnomebat.com>
2021-07-02 01:24:56 +0530

    Coming out as a Flask App 🌈️

---
+++ main.py
@@ -0,0 +1,80 @@
+from flask import Flask, render_template, request
+import git
+import datetime
+import os, shutil
+
+app = Flask(__name__)
+
+def getDates(year=None):
+    if year:
+        jan1 = datetime.datetime(year=year, month=1, day=1, hour=10, minute=20, second=59)
+    else:
+        jan1 = datetime.datetime.now() - datetime.timedelta(weeks=53)
+        jan1 -= datetime.timedelta(microseconds=jan1.microsecond)
+    def onDay(date, day): return date + datetime.timedelta(days=(day-date.weekday()) % 7)
+    first_sunday = onDay(jan1, 6)
+    dates = [list() for x in range(7)]
+    for x in range(52 * 7):
+        dates[x % 7].append(first_sunday + datetime.timedelta(x))
+    return dates
+
+@app.route('/', methods=['GET', 'POST'])
+def main():
+    if request.method == 'GET':
+        return render_template('main.html')
+    # a = [[int(request.form[f'{i} {j}']) for j in range(52)] for i in range(7)]
+    # text_to_render = request.form['text']
+    # font = Font('Fonts/subway-ticker.ttf', 8)
+    # try:
+    #     text = repr(font.render_text(text_to_render, 52, 7))
+    #     text_by_weekday = text.split('\n')
+    #     for i in range(7):
+    #         for j in range(52):
+    #             if text_by_weekday[i][j] == '#':
+    #                 a[i][j] = (a[i][j] + 1)%3
+    #     return render_template('main.html', a=a)
+    # except:
+    #     return "error"
+    a = [[int(request.form[f'{i} {j}']) for j in range(52)] for i in range(7)]
+    def getActiveDates(dates):
+        ad = []
+        for i in range(7):
+            for j in range(52):
+                for k in range(a[i][j]):
+                    ad.append(dates[i][j])
+        return ad
+    try:
+        year = int(request.form['year'])
+        dates = getActiveDates(getDates(year))
+    except:
+        dates = getActiveDates(getDates())
+    name = request.form['username']
+    email = request.form['email']
+    author = git.Actor(name, email)
+
+    repurl = "https://" + name + ":" + request.form['password'] + "@" + request.form['repo'][8:]
+    repname = repurl.split('/')[-1].split('.')[0]
+    if not os.path.isdir(repname):
+        try:
+            git.cmd.Git().clone(repurl)
+        except:
+            return render_template('result.html', msg='ERROR! Could not clone the repo. Ensure that the remote repo exists and that you have access to it.', form=request.form)
+    rep = git.Repo.init(repname)
+    nc = int(request.form['nc'])
+    for date in dates:
+        for n in range(nc):
+            rep.index.commit("made with love by gitfitti", author=author, committer=author, author_date=date.isoformat())
+    try:
+        rep.remotes.origin.set_url(repurl)
+    except:
+        rep.create_remote('origin', repurl)
+    try:
+        rep.remotes.origin.push()
+        shutil.rmtree(repname)
+    except:
+        shutil.rmtree(repname)
+        return render_template('result.html', msg='ERROR! Could not push to the repo. Ensure that the remote repo exists and that you have access to it.', form=request.form)
+    return render_template('result.html', msg=f"SUCCESS! Created {nc*len(dates)} commits as {name} [{email}] in {repname}", form=request.form)
+
+
+app.run()
---
+++ templates/script.js
@@ -0,0 +1,481 @@
+var txt = {
+    'a': Array(),
+    'b': Array(),
+    'c': Array(),
+    'd': Array(),
+    'e': Array(),
+    'f': Array(),
+    'g': Array(),
+    'h': Array(),
+    'i': Array(),
+    'j': Array(),
+    'k': Array(),
+    'l': Array(),
+    'm': Array(),
+    'n': Array(),
+    'o': Array(),
+    'p': Array(),
+    'q': Array(),
+    'r': Array(),
+    's': Array(),
+    't': Array(),
+    'u': Array(),
+    'v': Array(),
+    'w': Array(),
+    'x': Array(),
+    'y': Array(),
+    'z': Array(),
+    '.': Array(),
+    ',': Array(),
+    ';': Array(),
+    ':': Array(),
+    '\'': Array(),
+    '"': Array(),
+    ' ': Array(),
+    '+': Array(),
+    '-': Array(),
+    '_': Array(),
+    '[': Array(),
+    ']': Array(),
+    '(': Array(),
+    ')': Array(),
+    '<': Array(),
+    '>': Array(),
+    '!': Array(),
+    '?': Array(),
+    '*': Array()
+};
+txt['a'].push("    ");
+txt['a'].push(" aa ");
+txt['a'].push("a  a");
+txt['a'].push("aaaa");
+txt['a'].push("a  a");
+txt['a'].push("a  a");
+txt['a'].push("    ");
+
+txt['b'].push("    ");
+txt['b'].push("bbb ");
+txt['b'].push("b  b");
+txt['b'].push("bbbb");
+txt['b'].push("b  b");
+txt['b'].push("bbb ");
+txt['b'].push("    ");
+
+txt['c'].push("    ");
+txt['c'].push(" cc ");
+txt['c'].push("c  c");
+txt['c'].push("c   ");
+txt['c'].push("c  c");
+txt['c'].push(" cc ");
+txt['c'].push("    ");
+
+txt['d'].push("    ");
+txt['d'].push("ddd ");
+txt['d'].push("d  d");
+txt['d'].push("d  d");
+txt['d'].push("d  d");
+txt['d'].push("ddd ");
+txt['d'].push("    ");
+
+txt['e'].push("   ");
+txt['e'].push("eee");
+txt['e'].push("e  ");
+txt['e'].push("eee");
+txt['e'].push("e  ");
+txt['e'].push("eee");
+txt['e'].push("   ");
+
+txt['f'].push("   ");
+txt['f'].push("fff");
+txt['f'].push("f  ");
+txt['f'].push("fff");
+txt['f'].push("f  ");
+txt['f'].push("f  ");
+txt['f'].push("   ");
+
+txt['g'].push("    ");
+txt['g'].push(" ggg");
+txt['g'].push("g   ");
+txt['g'].push("g gg");
+txt['g'].push("g  g");
+txt['g'].push(" gg ");
+txt['g'].push("    ");
+
+txt['h'].push("    ");
+txt['h'].push("h  h");
+txt['h'].push("h  h");
+txt['h'].push("hhhh");
+txt['h'].push("h  h");
+txt['h'].push("h  h");
+txt['h'].push("    ");
+
+txt['i'].push(" ");
+txt['i'].push("i");
+txt['i'].push("i");
+txt['i'].push("i");
+txt['i'].push("i");
+txt['i'].push("i");
+txt['i'].push(" ");
+
+txt['j'].push("    ");
+txt['j'].push("   j");
+txt['j'].push("   j");
+txt['j'].push("   j");
+txt['j'].push("j  j");
+txt['j'].push(" jj ");
+txt['j'].push("    ");
+
+txt['k'].push("    ");
+txt['k'].push("k  k");
+txt['k'].push("k k ");
+txt['k'].push("kk  ");
+txt['k'].push("k k ");
+txt['k'].push("k  k");
+txt['k'].push("    ");
+
+txt['l'].push("   ");
+txt['l'].push("l  ");
+txt['l'].push("l  ");
+txt['l'].push("l  ");
+txt['l'].push("l  ");
+txt['l'].push("lll");
+txt['l'].push("   ");
+
+txt['m'].push("     ");
+txt['m'].push("m   m");
+txt['m'].push("mm mm");
+txt['m'].push("m m m");
+txt['m'].push("m   m");
+txt['m'].push("m   m");
+txt['m'].push("     ");
+
+txt['n'].push("     ");
+txt['n'].push("n   n");
+txt['n'].push("nn  n");
+txt['n'].push("n n n");
+txt['n'].push("n  nn");
+txt['n'].push("n   n");
+txt['n'].push("     ");
+
+txt['o'].push("    ");
+txt['o'].push(" oo ");
+txt['o'].push("o  o");
+txt['o'].push("o  o");
+txt['o'].push("o  o");
+txt['o'].push(" oo ");
+txt['o'].push("    ");
+
+txt['p'].push("    ");
+txt['p'].push("ppp ");
+txt['p'].push("p  p");
+txt['p'].push("ppp ");
+txt['p'].push("p   ");
+txt['p'].push("p   ");
+txt['p'].push("    ");
+
+txt['q'].push("    ");
+txt['q'].push(" qq ");
+txt['q'].push("q  q");
+txt['q'].push("q  q");
+txt['q'].push("q  q");
+txt['q'].push(" qq ");
+txt['q'].push("   q");
+
+txt['r'].push("    ");
+txt['r'].push("rrr ");
+txt['r'].push("r  r");
+txt['r'].push("rrr ");
+txt['r'].push("r r ");
+txt['r'].push("r  r");
+txt['r'].push("    ");
+
+txt['s'].push("    ");
+txt['s'].push(" sss");
+txt['s'].push("s   ");
+txt['s'].push(" ss ");
+txt['s'].push("   s");
+txt['s'].push("sss ");
+txt['s'].push("    ");
+
+txt['t'].push("   ");
+txt['t'].push("ttt");
+txt['t'].push(" t ");
+txt['t'].push(" t ");
+txt['t'].push(" t ");
+txt['t'].push(" t ");
+txt['t'].push("   ");
+
+txt['u'].push("    ");
+txt['u'].push("u  u");
+txt['u'].push("u  u");
+txt['u'].push("u  u");
+txt['u'].push("u  u");
+txt['u'].push(" uu ");
+txt['u'].push("    ");
+
+txt['v'].push("     ");
+txt['v'].push("v   v");
+txt['v'].push("v   v");
+txt['v'].push(" v v ");
+txt['v'].push(" v v ");
+txt['v'].push("  v  ");
+txt['v'].push("     ");
+
+txt['w'].push("     ");
+txt['w'].push("w   w");
+txt['w'].push("w w w");
+txt['w'].push("w w w");
+txt['w'].push("w w w");
+txt['w'].push(" w w ");
+txt['w'].push("     ");
+
+txt['x'].push("     ");
+txt['x'].push("x   x");
+txt['x'].push(" x x ");
+txt['x'].push("  x  ");
+txt['x'].push(" x x ");
+txt['x'].push("x   x");
+txt['x'].push("     ");
+
+txt['y'].push("     ");
+txt['y'].push("y   y");
+txt['y'].push(" y y ");
+txt['y'].push("  y  ");
+txt['y'].push("  y  ");
+txt['y'].push("  y  ");
+txt['y'].push("     ");
+
+txt['z'].push("   ");
+txt['z'].push("zzz");
+txt['z'].push("  z");
+txt['z'].push(" z ");
+txt['z'].push("z  ");
+txt['z'].push("zzz");
+txt['z'].push("   ");
+
+txt['.'].push(" ");
+txt['.'].push(" ");
+txt['.'].push(" ");
+txt['.'].push(" ");
+txt['.'].push(" ");
+txt['.'].push(".");
+txt['.'].push(" ");
+
+txt[','].push("  ");
+txt[','].push("  ");
+txt[','].push("  ");
+txt[','].push("  ");
+txt[','].push("  ");
+txt[','].push(" ,");
+txt[','].push(", ");
+
+txt[';'].push("  ");
+txt[';'].push("  ");
+txt[';'].push(" ;");
+txt[';'].push("  ");
+txt[';'].push(" ;");
+txt[';'].push("; ");
+txt[';'].push("  ");
+
+txt[':'].push(" ");
+txt[':'].push(" ");
+txt[':'].push(":");
+txt[':'].push(" ");
+txt[':'].push(":");
+txt[':'].push(" ");
+txt[':'].push(" ");
+
+txt['('].push("  ");
+txt['('].push(" (");
+txt['('].push("( ");
+txt['('].push("( ");
+txt['('].push("( ");
+txt['('].push(" (");
+txt['('].push("  ");
+
+txt[')'].push("  ");
+txt[')'].push(") ");
+txt[')'].push(" )");
+txt[')'].push(" )");
+txt[')'].push(" )");
+txt[')'].push(") ");
+txt[')'].push("  ");
+
+txt['['].push("  ");
+txt['['].push("[[");
+txt['['].push("[ ");
+txt['['].push("[ ");
+txt['['].push("[ ");
+txt['['].push("[[");
+txt['['].push("  ");
+
+txt[']'].push("  ");
+txt[']'].push("]]");
+txt[']'].push(" ]");
+txt[']'].push(" ]");
+txt[']'].push(" ]");
+txt[']'].push("]]");
+txt[']'].push("  ");
+
+txt['<'].push("   ");
+txt['<'].push("  <");
+txt['<'].push(" < ");
+txt['<'].push("<  ");
+txt['<'].push(" < ");
+txt['<'].push("  <");
+txt['<'].push("   ");
+
+txt['>'].push("   ");
+txt['>'].push(">  ");
+txt['>'].push(" > ");
+txt['>'].push("  >");
+txt['>'].push(" > ");
+txt['>'].push(">  ");
+txt['>'].push("   ");
+
+txt['!'].push(" ");
+txt['!'].push("!");
+txt['!'].push("!");
+txt['!'].push("!");
+txt['!'].push(" ");
+txt['!'].push("!");
+txt['!'].push(" ");
+
+txt['?'].push("    ");
+txt['?'].push("??? ");
+txt['?'].push("   ?");
+txt['?'].push(" ?? ");
+txt['?'].push("    ");
+txt['?'].push(" ?  ");
+txt['?'].push("    ");
+
+txt['+'].push("   ");
+txt['+'].push("   ");
+txt['+'].push(" + ");
+txt['+'].push("+++");
+txt['+'].push(" + ");
+txt['+'].push("   ");
+txt['+'].push("   ");
+
+txt['-'].push("  ");
+txt['-'].push("  ");
+txt['-'].push("  ");
+txt['-'].push("--");
+txt['-'].push("  ");
+txt['-'].push("  ");
+txt['-'].push("  ");
+
+txt['_'].push("   ");
+txt['_'].push("   ");
+txt['_'].push("   ");
+txt['_'].push("   ");
+txt['_'].push("   ");
+txt['_'].push("___");
+txt['_'].push("   ");
+
+txt['*'].push("     ");
+txt['*'].push("  *  ");
+txt['*'].push("* * *");
+txt['*'].push(" *** ");
+txt['*'].push("* * *");
+txt['*'].push("  *  ");
+txt['*'].push("     ");
+
+txt['\''].push(" ");
+txt['\''].push("'");
+txt['\''].push("'");
+txt['\''].push(" ");
+txt['\''].push(" ");
+txt['\''].push(" ");
+txt['\''].push(" ");
+
+txt[' '].push(" ");
+txt[' '].push(" ");
+txt[' '].push(" ");
+txt[' '].push(" ");
+txt[' '].push(" ");
+txt[' '].push(" ");
+txt[' '].push(" ");
+
+function toggle(elem) {
+    var $radios = $('input[type="radio"][name="' + $(elem).attr('name') + '"]');
+    var colorClasses = ["btn-secondary c0", "btn-secondary c1", "btn-secondary c2"];
+    var $checked = $radios.filter(':checked');
+    var $next = $radios.eq($radios.index($checked[0]) + 1);
+    if (!$next.length) {
+        $next = $radios.first();
+    }
+    $next.prop("checked", true);
+    var newValue = $radios.filter(':checked').val();
+    $(elem)
+        .removeClass(colorClasses.join(" "))
+        .addClass(colorClasses[newValue]);
+}
+
+function findw(text) {
+    var w = 0;
+    for (var c in text) {
+        if (text[c] in txt)
+            w += txt[text[c]][0].length + 1;
+    }
+    return w - 1;
+}
+
+function render(text) {
+    var w = findw(text);
+    if (w > 52)
+        alert("You typed too long");
+    else {
+        var offset = (((52 - w) / 2) >> 0);
+        for (var c in text) {
+            for (var k = 0; k < 7; k++)
+                for (var j = offset; j < offset + (txt[text[c]][j - offset].length); j++)
+                    if (txt[text[c]][k][j - offset] != ' ')
+                        toggle($('button[name="' + String(k) + ' ' + String(j) + '"]'));
+            offset += txt[text[c]][j - offset].length;
+            if (c != ' ')
+                offset++;
+        }
+    }
+}
+
+$('input[name="translate"]').click(function () {
+    var text = $('input[name="ttg"]').val();
+    render(text.toLowerCase());
+});
+
+$('input[name="clear"]').click(function () {
+    var $buttons = $('button');
+    for (i in $buttons) {
+        var $radio = $('input[type="radio"][name="' + $buttons[i].name + '"][value="0"]');
+        $radio.prop("checked", true);
+        $buttons[i].className = "btn btn-secondary c0";
+    }
+});
+
+$('input[name="invert"]').click(function () {
+    var buttons = $('button');
+    for (i in buttons)
+        toggle(buttons[i]);
+});
+
+let action = null;
+
+$("button[class=\"btn btn-secondary c0\"]").mousedown(function () {
+    toggle($(this));
+    action = "not null";
+});
+
+$(document).on("mouseup", function (event) {
+    action = null;
+});
+
+$("button[class=\"btn btn-secondary c0\"]").on("mouseup click", function (event) {
+    event.preventDefault();
+    action = null;
+});
+
+$("button[class=\"btn btn-secondary c0\"]").mouseenter(function (event) {
+    if (action)
+        toggle($(this));
+});
