c4b65ec1b500f49e85f76d4d62f0da43c464d6b3
heckerfr0d <hadif_b190513cs@nitc.ac.in>
2021-07-11 16:55:28 +0530

    oops

--- gitfitti/routes.py
+++ gitfitti/routes.py
@@ -6,9 +6,9 @@ from .utilities import *
 
 n = 0
 cont = []
-# DATABASE_URL = os.environ['DATABASE_URL']
-# conn = connect(DATABASE_URL, sslmode='require')
-conn = connect(dbname="gitfitti")
+DATABASE_URL = os.environ['DATABASE_URL']
+conn = connect(DATABASE_URL, sslmode='require')
+# conn = connect(dbname="gitfitti")
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
