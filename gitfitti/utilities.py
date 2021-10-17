247911a6ab24dd46a7f0131e24035e6750c3beeb
heckerfr0d <hadif_b190513cs@nitc.ac.in>
2021-10-17 17:31:15 +0530

    fix wrong url_for usage

--- gitfitti/utilities.py
+++ gitfitti/utilities.py
@@ -90,7 +90,7 @@ def editJS(alias, a):
             else:
                 txt[i] += ' '
     txt = "[\n\t'" + "',\n\t'".join(txt) + "'\n];"
-    with open(url_for('static', 'script.js'), 'a') as f:
+    with open('static/script.js', 'a') as f:
         f.write(f"\ntxt['{alias}'] = {txt}\n\n")
         if len(alias)>1:
             f.write(f"pub.push('{alias}');\n\n")
@@ -105,7 +105,7 @@ def openPR(name, alias, auth):
         'https://api.github.com/repos/heckerfr0d/gitfitti-web/forks', headers=headers)
     url = "https://api.github.com/repos/" + \
         name+"/gitfitti-web/contents/static/script.js"
-    base64content = base64.b64encode(open(url_for('static', 'script.js'), "rb").read())
+    base64content = base64.b64encode(open('static/script.js', "rb").read())
     data = requests.get(url+'?ref=main', headers=headers).json()
     sha = data['sha']
     message = json.dumps({"message": "Adding '" + alias + "' to js dict",
@@ -129,7 +129,7 @@ def merge(n, cont):
     git.cmd.Git().clone(
         f"https://heckerfr0d:{TOKEN}@github.com/heckerfr0d/gitfitti-web")
     repo = git.Repo.init('gitfitti-web')
-    with open(url_for('static', 'script.js'), 'rb') as fin:
+    with open('static/script.js', 'rb') as fin:
         with open('gitfitti-web/static/script.js', 'wb') as fout:
             shutil.copyfileobj(fin, fout)
     repo.git.add(update=True)
