b88d977d069cdf7be63d4f0c5326d2f1c2f3d220
heckerfr0d <hadif_b190513cs@nitc.ac.in>
2021-12-18 01:43:13 +0530

    back to docs lol

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: waitress-serve --listen "*:$PORT" --trusted-proxy '*' --trusted-proxy-headers 'x-forwarded-for x-forwarded-proto x-forwarded-port' --log-untrusted-proxy-headers --clear-untrusted-proxy-headers --threads ${WEB_CONCURRENCY:-4} wsgi:app
+worker: celery -A wsgi.celery worker --pool=solo --loglevel=info
--- gitfitti/__init__.py
+++ gitfitti/__init__.py
@@ -4,14 +4,14 @@ import os
 from celery import Celery
 
 login_manager = LoginManager()
-celery = Celery(__name__, broker=os.getenv('REDIS_TLS_URL'), result_backend=os.getenv('REDIS_TLS_URL'))
+celery = Celery(__name__, broker=os.getenv('REDIS_URL'), result_backend=os.getenv('REDIS_URL'))
 
 def init_app():
     app = Flask(__name__)
     app.config['DATABASE_URL'] = os.getenv('DATABASE_URL')
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-    app.config['CELERY_BROKER_URL'] = os.getenv('REDIS_TLS_URL')
-    app.config['CELERY_RESULT_BACKEND'] = os.getenv('REDIS_TLS_URL')
+    app.config['CELERY_BROKER_URL'] = os.getenv('REDIS_URL')
+    app.config['CELERY_RESULT_BACKEND'] = os.getenv('REDIS_URL')
     celery.conf.update(app.config)
     login_manager.init_app(app)
 
